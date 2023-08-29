184fe3bd3a2129f805eb26cff671f6a2e0a9be8d
akou97 <akourim97@gmail.com>
2023-08-29 10:12:39 +0200

    Model trainer added

---
+++ src/components/model_trainer.py
@@ -0,0 +1,80 @@
+import os, sys
+from dataclasses import dataclass
+
+from catboost import CatBoostRegressor
+from sklearn.ensemble import RandomForestRegressor,AdaBoostRegressor, GradientBoostingRegressor
+from sklearn.linear_model import LinearRegression
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.tree import DecisionTreeRegressor
+from xgboost import XGBRegressor
+from sklearn.metrics import  r2_score
+
+
+from src.exception import CustomException
+from src.logger import logging
+from src.utils import save_object, evaluate_models
+
+@dataclass
+class ModelTrainerConfig:
+    trained_model_file_path = os.path.join("artifacts", 'model.pkl')
+
+class ModelTrainer:
+    def __init__(self) -> None:
+        self.model_trainer_config = ModelTrainerConfig()
+
+    def initiate_model_trainer(self, train_arr, test_arr):
+        try:
+            logging.info("Split train and test input data")
+            X_train, y_train, X_test, y_test = (
+                train_arr[:,:-1],
+                train_arr[:,-1],
+                test_arr[:,:-1],
+                test_arr[:,-1]
+            )
+
+            models = {
+                "Random Forest": RandomForestRegressor(),
+                "Decision trees": DecisionTreeRegressor(),
+                "Gradient Boosting": GradientBoostingRegressor(), 
+                "Linear Regression": LinearRegression(), 
+                "KNeighbors Regressor": KNeighborsRegressor(),
+                "XGBoosting Regresor": XGBRegressor(),
+                "CatBoosting Regressor": CatBoostRegressor(),
+                "AdaBoosting Regressor" : AdaBoostRegressor()
+            }
+
+            model_report:dict=evaluate_models(X_train=X_train,y_train=y_train,
+                                              X_test=X_test,y_test=y_test,models=models )
+            
+            # Get best model score from model report
+            best_model_score = max(sorted(model_report.values()))
+
+            # Get best model name from dict
+            best_model_name = list(model_report.keys())[
+                list(model_report.values()).index(best_model_score)
+            ]
+
+            best_model = models[best_model_name]
+
+            thd_score = 0.6
+
+            if best_model_score < thd_score:
+                raise CustomException(f"No best model founed : best accuracy is {best_model_score} < {thd_score}")
+            logging.info("Best model found on both train and test datasets")
+            logging.info(f"Best model = {best_model_name}, and score = {best_model_score}")
+
+            # save best model
+            save_object(
+                file_path=self.model_trainer_config.trained_model_file_path,
+                obj = best_model
+            )
+
+            predicted = best_model.predict(X_test)
+            r2_square = r2_score(y_test, predicted)
+
+            return r2_square
+
+        except Exception as e:
+            raise CustomException(e, sys)
+
+
--- src/utils.py
+++ src/utils.py
@@ -2,7 +2,7 @@ import os, sys
 import numpy as np
 import pandas as pd
 from src.exception import CustomException
-
+from sklearn.metrics import r2_score
 import dill
 def save_object(file_path, obj):
     try:
@@ -10,5 +10,23 @@ def save_object(file_path, obj):
         os.makedirs(dir_path, exist_ok=True)
         with open(file_path, 'wb') as f:
             dill.dump(obj, f)
+    except Exception as e:
+        raise CustomException(e, sys)
+    
+def evaluate_models(X_train, y_train , X_test, y_test, models):
+    try:
+        report = {}
+        for name_model, model in models.items():
+            model.fit(X_train, y_train)
+            y_train_pred  = model.predict(X_train)
+            y_test_pred = model.predict(X_test)
+
+            train_model_score = r2_score(y_train, y_train_pred)
+            test_model_score = r2_score(y_test, y_test_pred)
+            
+            report[name_model] = test_model_score
+        
+        return report
+    
     except Exception as e:
         raise CustomException(e, sys)
