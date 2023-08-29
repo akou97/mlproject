63e6bd7ffe2773041eb3b52d674da3fab3cb5e4c
akou97 <akourim97@gmail.com>
2023-08-29 10:34:26 +0200

    Hyperparameters optimisation added

--- src/utils.py
+++ src/utils.py
@@ -3,6 +3,7 @@ import numpy as np
 import pandas as pd
 from src.exception import CustomException
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 import dill
 def save_object(file_path, obj):
     try:
@@ -13,11 +14,18 @@ def save_object(file_path, obj):
     except Exception as e:
         raise CustomException(e, sys)
     
-def evaluate_models(X_train, y_train , X_test, y_test, models):
+def evaluate_models(X_train, y_train , X_test, y_test, models, params):
     try:
         report = {}
         for name_model, model in models.items():
+            model_params = params[name_model]
+
+            gs = GridSearchCV(model, model_params, cv=4)
+            gs.fit(X_train, y_train)
+
+            model.set_params(**gs.best_params_)
             model.fit(X_train, y_train)
+            
             y_train_pred  = model.predict(X_train)
             y_test_pred = model.predict(X_test)
 
